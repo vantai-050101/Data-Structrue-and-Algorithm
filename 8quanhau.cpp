9df7057b0ab292777ae7cdc499358f42c9f3d4eb
Van Tai <vantai.050101@gmail.com>
2020-08-06 13:37:23 +0700

    Hoan vi to hop, Ma di tuan, 8 quan hau, Qua trung

--- 8quanhau.cpp
+++ 8quanhau.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
-#define MAX 8
+#include <string>
+#define MAX 6
 using namespace std;
-int x[] = { -2,-1,1,2,2,1,-1,-2 };
-int y[] = { 1,2,2,1,-1,-2,-2,-1 };
+// init and print Matrix
 int matrix[MAX][MAX] = { 0 };
-
-void printMatrix() {
-	for (int i = 0; i < MAX; i++) {
-		for (int j = 0; j < MAX; j++)
-			cout << matrix[i][j] << '\t';
+void printMatrix(int n = MAX, int m = MAX) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++)
+			cout << matrix[i][j] << " ";
 		cout << '\n';
 	}
 }
 
-void go(int i, int j, int count) {
+// Ma di tuan
+int x[] = { -2,-1,1,2,2,1,-1,-2 };
+int y[] = { 1,2,2,1,-1,-2,-2,-1 };
+void go(int i = 0, int j = 0, int count = 0) {
 	if (count == MAX * MAX) {
 		matrix[i][j] = count;
 		printMatrix();
-
-		char c;
-		cout << "Do you want to print next (8 quan hau) macth Matrix: y/n? ";
-		cin >> c;
-		if (c == 'y');
-		else if ('n')
-			exit(0);
 		cout << '\n';
 	}
 	else {
@@ -40,13 +35,85 @@ void go(int i, int j, int count) {
 	}
 }
 
-int main() {
-	int i, j;
-	cout << "Started point (x,y)\n";
-	cout << '\t' << "x: "; cin >> i;
-	cout << '\t' << "y: "; cin >> j;
+// Hoan vi to hop
+void hoanvi(string st = "") {
+	if (st.size() == MAX)
+		cout << st << '\n';
+	else {
+		for (int i = 1; i <= MAX; i++) {
+			if (st.find(to_string(i), 0) >= st.size()) {
+				st = st + to_string(i);
+				hoanvi(st);
+				st.pop_back();
+			}
+		}
+	}
+}
 
-	go(i, j, 1);
-	
+// Tam quan hau
+#define N 6
+#define M 6
+bool try_go(int count, int i) {
+	for (int j = 1; j<=count; j++)
+		if (matrix[count-j][i] != 0 or
+			(0 <= count - j and 0 <= i - j and matrix[count - j][i - j] != 0) or
+			(0 <= count - j and i + j < M and matrix[count - j][i + j] != 0))
+			return false;
+	return true;
+}
+void tamQuanhau(int count = 0) {
+	if (count == N) {
+		printMatrix(N, M);
+		cout << '\n';
+	}
+	else {
+		for (int i = 0; i < M; i++)
+			if (try_go(count, i) == true) {
+				matrix[count][i] = 1;
+				tamQuanhau(count + 1);
+				matrix[count][i] = 0;
+			}
+	}
+}
+
+// Qua trung
+bool try_(int count, int k) {
+	int c = 0;
+	for (int i = 0; i < count; i++) {
+		if (matrix[i][k] != 0)
+			c++;
+		if (c > 1)
+			return false;
+	}
+	return true;
+}
+void quatrung(int count = 0) {
+	if (count == N) {
+		printMatrix(N, M);
+		cout << '\n';
+	}
+	else {
+		for (int i = 0; i < M - 1; i++) {
+			for (int j = i + 1; j < M; j++)
+				if (try_(count, i) and try_(count, j)) {
+					matrix[count][i] = 1;
+					matrix[count][j] = 1;
+					quatrung(count + 1);
+					matrix[count][i] = 0;
+					matrix[count][j] = 0;
+				}
+		}
+	}
+}
+
+int main() {
+	cout << "\nHoan vi to hop: ";
+	hoanvi();
+	cout << "\nMa di tuan: ";
+	go();
+	cout << "\nTam quan hau: ";
+	tamQuanhau();
+	cout << "\nQua trung: ";
+	quatrung();
 	return 0;
 }
